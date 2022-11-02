0d3731c9da58c5eb7f73f33a7ce92b58da7ad518
Jully Ketely <42702863+jullysilva@users.noreply.github.com>
2022-11-02 12:37:59 -0300

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ app.use((req, res, next) => {
   //Quais são os métodos que a conexão pode realizar na API
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH");
   app.use(cors());
-  next();
 });
 
 app.use(express.json());
