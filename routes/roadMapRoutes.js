1a3b62f0d0ea647fade3e83d84c7caa99a1b5bfe
Ian Cunha <61791519+IanCunha@users.noreply.github.com>
2022-11-20 19:54:23 -0300

    atualizar lista participantes roteiro

--- routes/roadMapRoutes.js
+++ routes/roadMapRoutes.js
@@ -149,6 +149,31 @@ router.patch("/:id", async (req, res) => {
   }
 });
 
+//atualizar Participantes de um Roteiro
+router.patch("/:id", async (req, res) => {
+  const id = req.params.id;
+
+  const { participants, person } = req.body;
+
+  const roadmap = {
+    participants,
+    person,
+  };
+
+  try {
+    const updatedParticipants = await RoadMap.updateOne({ _id: id }, roadmap);
+
+    if (updatedParticipants.matchedCount === 0) {
+      res.status(422).json({ message: "Roteiro não encontrado" });
+      return;
+    }
+
+    res.status(200).json(roadmap);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+});
+
 //Apagar um Roteiro
 router.delete("/:id", async (req, res) => {
   const id = req.params.id;
