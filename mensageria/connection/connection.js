9438644f1a99c86fdb662f1036645b8e3e9477d2
jullysilva <jully.silva@dtidigital.com.br>
2022-12-14 14:18:15 -0300

    reform

--- mensageria/connection/connection.js
+++ mensageria/connection/connection.js
@@ -1,7 +1,8 @@
+#!/usr/bin/env node
 var amqp = require("amqplib/callback_api");
 module.exports = (callback) => {
   amqp.connect(
-    "amqp://user:user@whateverhost:whateverport",
+    "amqps://kcxhzzfu:st4kIkVZpGgzrTtRqjkzK3Ews-97uo8v@hawk.rmq.cloudamqp.com/kcxhzzfu",
     (error, conection) => {
       if (error) {
         throw new Error(error);
--- mensageria/connection/index.js
+++ mensageria/connection/index.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 var rabbitMQHandler = require("./connection");
 
 module.exports = rabbitMQHandler;
